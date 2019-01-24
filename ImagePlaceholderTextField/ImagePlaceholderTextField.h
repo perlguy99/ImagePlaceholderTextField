d6fb974e5ca721ab1084fde6c5081064fe6adfb3
Brent Michalski <Brent.Michalski@gmail.com>
2019-01-24 15:54:38 -0600

    Initial Commit

---
+++ ImagePlaceholderTextField/ImagePlaceholderTextField.h
@@ -0,0 +1,19 @@
+//
+//  ImagePlaceholderTextField.h
+//  ImagePlaceholderTextField
+//
+//  Created by Brent Michalski on 1/24/19.
+//  Copyright © 2019 Perlguy, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImagePlaceholderTextField.
+FOUNDATION_EXPORT double ImagePlaceholderTextFieldVersionNumber;
+
+//! Project version string for ImagePlaceholderTextField.
+FOUNDATION_EXPORT const unsigned char ImagePlaceholderTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImagePlaceholderTextField/PublicHeader.h>
+
+
